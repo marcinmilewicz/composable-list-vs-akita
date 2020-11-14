21f41abe27d5718f1de88c92c74356a4a8e4e344
marcinmilewicz <marcinmilewicz87@gmail.com>
2020-11-14 16:24:58 +0100

    1 file modified

--- src/app/feature-people/feature-people.module.ts
+++ src/app/feature-people/feature-people.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PeopleListComponent } from './containers/people-list/people-list.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, ReactiveFormsModule
   ],
   exports: [
     PeopleListComponent
