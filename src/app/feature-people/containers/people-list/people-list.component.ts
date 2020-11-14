1b6333dadac6dad1ea1fd69f63e858316e3ac26c
marcinmilewicz <marcinmilewicz87@gmail.com>
2020-11-14 17:54:19 +0100

    1 file modified

--- src/app/feature-people/containers/people-list/people-list.component.ts
+++ src/app/feature-people/containers/people-list/people-list.component.ts
@@ -15,10 +15,10 @@ const createPaginationStream = <State, Parameters extends Record<string, object>
     callback: (params: Parameters[keyof Parameters][]) => () => Observable<PaginationResponse<getEntityType<State>>>,
     paginatorRef: PaginatorPlugin<State>
 ) =>
-    combineLatest(fieldChanges).pipe(
+    combineLatest([...fieldChanges, paginatorRef.pageChanges]).pipe(
         tap(() => paginatorRef.clearCache()),
         switchMap((values: Parameters[keyof Parameters][]) => {
-
+console.log(values)
             const requestFunction = callback(values)
 
             return paginatorRef.getPage(requestFunction) as Observable<PaginationResponse<getEntityType<State>>>;
