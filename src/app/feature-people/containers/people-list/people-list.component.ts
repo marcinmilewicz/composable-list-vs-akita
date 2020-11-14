ae3ade6051377abb2bf667487b8e47b0f896e46d
marcinmilewicz <marcinmilewicz87@gmail.com>
2020-11-14 18:42:03 +0100

    1 file modified, 1 added

--- src/app/feature-people/containers/people-list/people-list.component.ts
+++ src/app/feature-people/containers/people-list/people-list.component.ts
@@ -8,22 +8,7 @@ import { FormBuilder } from "@angular/forms";
 import { untilDestroyed } from "ngx-take-until-destroy";
 import { startWith, switchMap, tap } from "rxjs/operators";
 import { PeopleState } from "../../state/people.store";
-
-
-const createPaginationStream = <State, Parameters extends Record<string, object>>(
-    fieldChanges: Observable<Parameters[keyof Parameters]>[],
-    callback: (params: Parameters[keyof Parameters][]) => () => Observable<PaginationResponse<getEntityType<State>>>,
-    paginatorRef: PaginatorPlugin<State>
-) =>
-    combineLatest([...fieldChanges, paginatorRef.pageChanges]).pipe(
-        tap(() => paginatorRef.clearCache()),
-        switchMap((values: Parameters[keyof Parameters][]) => {
-console.log(values)
-            const requestFunction = callback(values)
-
-            return paginatorRef.getPage(requestFunction) as Observable<PaginationResponse<getEntityType<State>>>;
-        })
-    );
+import { createPaginationStream } from "../../../shared/shared-state/akita-pagination";
 
 
 @Component({
